6f8df275f6cc1a3d01a1585e086f501a61c37c5d
Andriy V. Koval <koval.andrey@gmail.com>
2020-05-30 11:21:32 -0400

    Update ellis-covid.R

--- manipulation/ellis-covid.R
+++ manipulation/ellis-covid.R
@@ -1,58 +1,67 @@
-# Source
+# Data Source
 # https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
-
-
-
 rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run.
-# This is not called by knitr, because it's above the first chunk.
 cat("\f") # clear console when working in RStudio
 
 # ---- load-sources ------------------------------------------------------------
-# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
-
 
 # ---- load-packages -----------------------------------------------------------
-# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(magrittr) #Pipes
-library(utils)
-library(httr)
-library(magrittr)
+library(readr)
 library(dplyr)
+library(fs)
 
 # ---- declare-globals ---------------------------------------------------------
-config <- config::get()
+config   <- config::get()
 path_url <- "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
 
-# ---- load-data ---------------------------------------------------------------
 
-#download the dataset from the ECDC website to a local temporary file
-GET(url = path_url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".csv")))
-ds_ocdc_raw <- read.csv(tf)
-if (!fs::dir_exists(fs::path_dir(config$path_input_covid))) fs::dir_create(fs::path_dir(config$path_input_covid))
-readr::write_csv(ds_ocdc_raw, config$path_input_covid)
-checkmate::assert_file(config$path_input_covid)
+# ---- load-data ---------------------------------------------------------------
+# # download the dataset from the ECDC website to a local temporary file
+# ds_ocdc_raw <- readr::read_csv(path_url)
+#
+# # to create the folder if it does not exist
+# if( !fs::dir_exists(  fs::path_dir(config$path_input_covid)  ) ){
+#   fs::dir_create( fs::path_dir(config$path_input_covid) )
+# }
+# ds_ocdc_raw %>% readr::write_csv(config$path_input_covid) # to save a local version
+# checkmate::assert_file(config$path_input_covid) # to verify success
+# rm(ds_ocdc_raw)
 # # run above line once per update
 
+# input local saved file for the current date
 ds_covid <- readr::read_csv(config$path_input_covid)
 ds_covid %>% glimpse()
 
-# ---- reconcile-countries -------------------
-ds_country <-
-  readr::read_csv(
-    config$path_country
-  ) %>%
-  dplyr::filter(desired)
-
-ds_country <-
-  ds_country %>%
-  dplyr::left_join(
-    ds_covid %>%
-      dplyr::distinct(countriesAndTerritories,countryterritoryCode),
-    by = c("id" = "countryterritoryCode")
-  )
-# sources can be joined by the three letter country code
 # ---- tweak-data -----------------------
-names(ds_covid) <- c("date", "day", "month", "year", "n_cases", "n_deaths", "country", "geo_id", "country_code","n_population_2018", "continent")
+names(ds_covid) <- names(ds_covid) %>% snakecase::to_snake_case()
+
+ds_covid <- ds_covid %>%
+  dplyr::rename(
+    "date"               = "date_rep"      # because shorter
+    ,"n_cases"           = "cases"         # because count people
+    ,"n_deaths"          = "deaths"        # because count people
+    ,"n_population_2018" = "pop_data_2018" # because count people
+
+    ,"country_label"     = "countries_and_territories"
+    ,"country_code"      = "countryterritory_code"
+    ,"country_code2"     = "geo_id"
+    ,"continent_label"   = "continent_exp"
+) %>%
+  dplyr::select(-day, -month, -year) %>% # because derivative from date
+  dplyr::select(date,n_cases, n_deaths, n_population_2018, dplyr::everything()) %>%
+  dplyr::mutate(
+    date = lubridate::dmy(date)
+  )
+ds_covid %>% glimpse()
+
+ds_covid %>% pull(country_code) %>% unique()
+ds_covid %>% pull(country_code2) %>% unique()
+
+d <- ds_covid %>% filter(country_code == "N/A")
+
+d <- ds_covid %>% filter(country_code == "NAM") %>%
+d
 
 ds_covid <- ds_covid %>%
   dplyr::mutate(
